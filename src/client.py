50f79d712f382d1102d049deaf3580d6bae09fc8
felixschorer <felixschorer@users.noreply.github.com>
2019-12-18 09:31:32 +0100

    Remove loop.stop() call in protocol, use future instead

--- src/client.py
+++ src/client.py
@@ -1,36 +1,32 @@
 import asyncio
 import secrets
-from asyncio import DatagramProtocol
-from functools import partial
+from asyncio import DatagramProtocol, Future
 
 from packets import ResourceRequest
 
 
 class ClientProtocol(DatagramProtocol):
-    def __init__(self, loop):
-        self.loop = loop
+    def __init__(self):
+        self.complete_future = Future()
 
     def connection_made(self, transport):
         resource_request = ResourceRequest(connection_id=secrets.token_bytes(16),
                                            resource_id=secrets.token_bytes(16),
                                            offset=0)
         transport.sendto(resource_request.to_bytes())
-
         transport.close()
 
     def connection_lost(self, _):
-        self.loop.stop()
+        self.complete_future.done()
 
 
 def main():
     loop = asyncio.get_event_loop()
-    client = partial(ClientProtocol, loop)
 
-    connect = loop.create_datagram_endpoint(client, remote_addr=('127.0.0.1', 9999))
+    connect = loop.create_datagram_endpoint(ClientProtocol, remote_addr=('127.0.0.1', 9999))
     transport, protocol = loop.run_until_complete(connect)
 
-    loop.run_forever()
-    transport.close()
+    loop.run_until_complete(protocol.complete_future)
     loop.close()
 
 
