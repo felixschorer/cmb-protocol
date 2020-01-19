641dd2d7f3149bcfc3d8a668e82542671d978f83
Felix Schorer <felixschorer@users.noreply.github.com>
2020-01-19 13:13:45 +0100

    Add doc strings to TFRC stubs

--- cmb_protocol/tfrc.py
+++ cmb_protocol/tfrc.py
@@ -1,9 +1,21 @@
 class TFRCSender:
     def update(self, delay, timestamp, receive_rate, loss_event_rate):
-        
+        """
+        :param delay:
+        :param timestamp:
+        :param receive_rate:
+        :param loss_event_rate:
+        :return:
+        """
         return 0, 0
 
 
 class TFRCReceiver:
     def update(self, timestamp, estimated_rtt, sequence_number):
+        """
+        :param timestamp:
+        :param estimated_rtt:
+        :param sequence_number:
+        :return:
+        """
         return 0, 0, 0, 0.0
