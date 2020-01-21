69e13a3e65559b351aa117de1e369a527619e129
Felix Schorer <felixschorer@users.noreply.github.com>
2020-01-21 22:01:29 +0100

    Raise NotImplemented to be in line with the Python Data Model

--- cmb_protocol/timestamp.py
+++ cmb_protocol/timestamp.py
@@ -1,11 +1,12 @@
 import trio
-
 from cmb_protocol.helpers import unpack_uint24, pack_uint24
 
 
 class Timestamp:
+    MAX_VALUE = 2**24 / 1000  # 24 bit, in seconds, millisecond accuracy
+
     def __init__(self, value):
-        self.value = value % (2 ** 24 / 1000)
+        self.value = value % self.MAX_VALUE
 
     @staticmethod
     def now():
@@ -22,30 +23,33 @@ class Timestamp:
         if isinstance(other, int) or isinstance(other, float):
             return Timestamp(self.value + other)
         else:
-            raise TypeError('only supported for int and float')
+            raise NotImplemented('only supported for int and float')
+
+    def __radd__(self, other):
+        return self + other
 
     def __sub__(self, other):
         if isinstance(other, int) or isinstance(other, float):
             return Timestamp(self.value - other)
         elif isinstance(other, Timestamp):
-            return (self.value - other.value) % (2 ** 24 / 1000)
+            return (self.value - other.value) % self.MAX_VALUE
         else:
-            raise TypeError('only supported for int and float or Timestamp')
+            raise NotImplemented('only supported for int and float or Timestamp')
 
     def __eq__(self, other):
         return isinstance(other, Timestamp) and self.value == other.value
 
     def __lt__(self, other):  # self < other (self older than other)
         if isinstance(other, Timestamp):
-            return self - other > other - self
+            return self - other > other - self  # consider shorter duration to be more likely
         else:
-            raise TypeError('only supported for Timestamp')
+            raise NotImplemented('only supported for Timestamp')
 
     def __gt__(self, other):  # self > other (self newer than other)
         if isinstance(other, Timestamp):
-            return self - other < other - self
+            return self - other < other - self  # consider shorter duration to be more likely
         else:
-            raise TypeError('only supported for Timestamp')
+            raise NotImplemented('only supported for Timestamp')
 
     def __le__(self, other):
         return self == other or self < other
