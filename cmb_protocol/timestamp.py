5b4ec72b7b1eff33e48d523af30f41689dc66d79
Felix Schorer <felixschorer@users.noreply.github.com>
2020-01-21 22:46:58 +0100

    Fix raising NotImplemented & add better comments

--- cmb_protocol/timestamp.py
+++ cmb_protocol/timestamp.py
@@ -3,6 +3,14 @@ from cmb_protocol.helpers import unpack_uint24, pack_uint24
 
 
 class Timestamp:
+    """
+    Timestamp which can be represented in 24 bits, measured in seconds with millisecond precision.
+    It will wrap around after approx 4:40h.
+
+    All timestamps are relative to the clock of the current trio context. Since trio uses randomized clocks,
+    timestamps must not be compared with timestamps created in a different trio context.
+    """
+
     MAX_VALUE = 2**24 / 1000  # 24 bit, in seconds, millisecond accuracy
 
     def __init__(self, value):
@@ -23,7 +31,7 @@ class Timestamp:
         if isinstance(other, int) or isinstance(other, float):
             return Timestamp(self.value + other)
         else:
-            raise NotImplemented('only supported for int and float')
+            return NotImplemented
 
     def __radd__(self, other):
         return self + other
@@ -34,22 +42,30 @@ class Timestamp:
         elif isinstance(other, Timestamp):
             return (self.value - other.value) % self.MAX_VALUE
         else:
-            raise NotImplemented('only supported for int and float or Timestamp')
+            return NotImplemented
 
     def __eq__(self, other):
         return isinstance(other, Timestamp) and self.value == other.value
 
     def __lt__(self, other):  # self < other (self older than other)
         if isinstance(other, Timestamp):
-            return self - other > other - self  # consider shorter duration to be more likely
+            # Due to wrap around at approx. 4:40h, there is no way of knowing which timestamp is older.
+            # However, we can assume that the order which produces the smallest duration to be correct.
+            # This works in our case as we won't be comparing timestamps with each other
+            # which are apart by more than a few seconds.
+            return self - other > other - self
         else:
-            raise NotImplemented('only supported for Timestamp')
+            return NotImplemented
 
     def __gt__(self, other):  # self > other (self newer than other)
         if isinstance(other, Timestamp):
-            return self - other < other - self  # consider shorter duration to be more likely
+            # Due to wrap around at approx. 4:40h, there is no way of knowing which timestamp is older.
+            # However, we can assume that the order which produces the smallest duration to be correct.
+            # This works in our case as we won't be comparing timestamps with each other
+            # which are apart by more than a few seconds.
+            return self - other < other - self
         else:
-            raise NotImplemented('only supported for Timestamp')
+            return NotImplemented
 
     def __le__(self, other):
         return self == other or self < other
